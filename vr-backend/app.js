d5ca8f0578202aad1a340f8cf390e6a4ebd5f2be
Vishal-sikchi-088 <vishalsikchi088@gmail.com>
2024-11-09 16:24:58 +0530

    cors has been installed

--- vr-backend/app.js
+++ vr-backend/app.js
@@ -1,10 +1,13 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+const cors = require('cors')
 const vehicleRoutes = require('./routes/vehicleRoutes')
 const bookingRoutes = require('./routes/bookingRoutes')
 
 const app = express()
+app.use(cors())
 app.use(bodyParser.json())
+
 
 app.use('/api', vehicleRoutes)
 app.use('/api', bookingRoutes)
